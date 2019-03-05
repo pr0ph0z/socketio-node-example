1d765b9ac08a128448566e5878406e3ca1e1119a
Mohamad Radisha <pr0ph0z23@gmail.com>
2019-03-05 16:47:04 +0700

    Commit changes in client part 1

--- cli/Part-01/client.js
+++ cli/Part-01/client.js
@@ -2,6 +2,8 @@ var io = require('socket.io-client')
 var socket = io('http://localhost:3000');
 var stdin = process.openStdin();
 
+process.stdout.write('Your message: ')
 stdin.addListener("data", function(d) {
+    process.stdout.write('Your message: ')
     socket.emit('chat message', d.toString().trim())
 });
